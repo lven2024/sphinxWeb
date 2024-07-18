a72a8fe43523c05cf4ef336ce06903354d3a255b
²²ÂÎÒÊÇ˭ <448592111@qq.com>
2024-07-18 10:36:24 +0800

    sphinx

---
+++ source/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sphinxWeb'
+copyright = '2024, lven2024'
+author = 'lven2024'
+release = 'v2.1.6'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+
+html_logo = "./_static/logo.png"
+html_static_path = ['_static']
+html_js_files = [
+    'my_custom.js',
+]
