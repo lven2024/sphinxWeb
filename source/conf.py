585016e6df6446b8a259824148c62d05e15672e6
²²ÂÎÒÊÇ˭ <448592111@qq.com>
2024-07-18 16:25:38 +0800

    111

--- source/conf.py
+++ source/conf.py
@@ -6,49 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sphinxWeb'
-copyright = '2024, lven2024'
-author = 'lven2024'
-release = 'v2.1.6'
+project = '在线阅读'
+copyright = '2024, lven'
+author = 'lven'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+#extensions = []
 extensions = ['recommonmark','sphinx_markdown_tables']
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
-
-myst_enable_extensions = [
-    "tasklist",
-    "deflist",
-    "dollarmath",
-]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh_CN'
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'analytics_anonymize_ip': False,
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-}
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_logo = "./_static/logo.png"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_js_files = [
-    'my_custom.js',
-]
